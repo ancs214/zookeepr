dcfd09875fcecd5f55c24b9071ad916cb8a655d4
ancs214 <ancs214@gmail.com>
2022-04-13 10:47:34 -0400

    added ability to save post req data to animals.json file. added post req validation function

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+//NOTE: animals.json is an object, with an array (animals) of objects (id, name, species, diet....etc)
 
+const fs = require('fs');
+const path = require('path');
 const { animals } = require('./data/animals');
 const express = require('express');
 //tells heroku to use the environment variable they have set, if not default to 80
@@ -6,9 +9,12 @@ const PORT = process.env.PORT || 3001;
 //assign express() to the app variable so that we can later chain on methods to the Express.js server.
 const app = express();
 
-// parse incoming string or array data that will come from a POST request
+//MIDDLEWARE
+// parse incoming string or array data from a POST request and parse into the req.body object
+//converts incoming POST data and converts to key/value pairings that can be accessed in the req.body object
+//extended.true option informs our server that there may be sub-array data nested in it as well, so it needs to look as deep into POST data as possible to parse correctly 
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
+// method to parse incoming JSON data before getting to the endpoint
 app.use(express.json());
 
 //get method requires two arguments: 1. a string that describes the route the client will have to fetch from 2. a callback func that will execute every time that route is accessed w a GET request
@@ -70,6 +76,25 @@ app.listen(PORT, () => {
     return result;
   }
 
+  //func that accepts POST route's req.body value and the array we want to add the data to (animalsArray) so that we can add a new animal to the catalog
+  function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    //add req.body data to animalsArray
+    animalsArray.push(animal);
+
+    //writeFileSync is the synchronous version of writeFile and doesnt require a callback func (ok since this is smaller file)
+    fs.writeFileSync(
+      //__dirname = folder of the file we execute the code in that contains path to animals.json
+      //join method will join value of __dirname with animals.json
+      path.join(__dirname, './data/animals.json'),
+      //convert JS array data to JSON, null = we dont want to edit any of our existing data, 2 = we want to create white space between our values to make it more readable
+      JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+  
+    // return finished code to post route for response
+    return animal;
+  }
+
   //GET ROUTE/ENDPOINT
   //get method requires path and req/res arguments
   //req.query - often combines MULTIPLE PARAMTERS
@@ -104,6 +129,33 @@ app.listen(PORT, () => {
   //post req indicate a client requesting the server to accept data instead of vice versa
   app.post('/api/animals', (req, res) => {
     //req.body is where our INCOMING content will be. this can be risky accepting data, which is why most APIs require user authentication. validation libraries (expreess-validator npm) on the server side also ensure data meets certain criteria.
-    console.log(req.body);
-    res.json(req.body);
+    
+    //set an id based on what the next index of the array will be; will work only as long as we dont remove any data
+    req.body.id = animals.length.toString();
+
+    //run validation function
+    if (!validateAnimal(req.body)) {
+      //if any return false, user will get 400 error (server has no problems and understands request, but request was incorrectly made) with message below:
+      res.status(400).send('The animal is not properly formatted.');
+    } else {
+      const animal = createNewAnimal(req.body, animals);
+      res.json(animal);
+    }
   });
+
+  //POST request validation: must have name, species, diet, personality data entered and must be a string
+  function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
